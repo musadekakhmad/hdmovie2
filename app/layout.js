6f5ad4a100374c782313f827e2d6255f913c2bd6
musadek akhmad <musadekakhmad@gmail.com>
2025-08-16 09:12:05 +0700

    Update layout.js
    w

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,10 @@
+// ---------------------------------------------------------------- //
+// Nama File: app/layout.js
+// Fungsi: Merupakan layout utama untuk seluruh halaman web,
+//         digunakan untuk mengatur elemen-elemen yang tampil
+//         di semua halaman, seperti header, footer, dan lebar konten.
+// ---------------------------------------------------------------- //
+
 import './globals.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -5,20 +12,20 @@ import AdsterraLayoutWrapper from '../components/AdsterraLayoutWrapper'; // Impo
 // Menghapus import 'video.js/dist/video-js.css'; dari sini
 
 export const metadata = {
-  title: 'Estrenoya | Free HD Movie & TV Show Streaming',
+  title: 'Cineflix | Free HD Movie & TV Show Streaming',
   description: 'Your ultimate destination for high-quality, free movie and TV show streaming..',
   // Meta tag Open Graph untuk Facebook
   openGraph: {
-    title: 'Estrenoya | Free HD Movie & TV Show Streaming',
+    title: 'Cineflix | Free HD Movie & TV Show Streaming',
     description: 'Your ultimate destination for high-quality, free movie and TV show streaming..',
     url: 'https://estrenoya.netlify.app/',
-    siteName: 'Estrenoya',
+    siteName: 'Cineflix',
     images: [
       {
         url: 'https://live.staticflickr.com/65535/54707174696_49edde76e3_b.jpg',
         width: 1200,
         height: 630,
-        alt: 'Estrenoya',
+        alt: 'Cineflix',
       },
     ],
     locale: 'en_US',
@@ -31,7 +38,7 @@ export const metadata = {
     card: 'summary_large_image',
     site: '@WatchStream123', // User Twitter Anda
     creator: '@WatchStream123',
-    title: 'Estrenoya | Free HD Movie & TV Show Streaming',
+    title: 'Cineflix | Free HD Movie & TV Show Streaming',
     description: 'Your ultimate destination for high-quality, free movie and TV show streaming..',
     images: ['https://live.staticflickr.com/65535/54707174696_49edde76e3_b.jpg'], // Ganti dengan URL gambar yang sesuai
   },
@@ -46,7 +53,9 @@ export default function RootLayout({ children }) {
         <Header />
         {/* AdsterraLayoutWrapper membungkus children (semua konten halaman) */}
         <AdsterraLayoutWrapper>
-          {children}
+          <div className="mx-auto max-w-7xl">
+            {children}
+          </div>
           <Footer />
         </AdsterraLayoutWrapper>
       </body>
